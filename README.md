57654d304b58fa9beb406b180f23a97c2403bdc1
Devika <devikanair3999@gmail.com>
2021-05-16 20:47:37 +0530

    Creating Initial Coursera Repository

---
+++ README.md
@@ -0,0 +1,6 @@
+Coursera Assignment 1
+
+Course Name : Introduction to Embedded Systems Software and Architecture
+Author Name : Devika Nair
+
+Create a simple application that performs statistical analytics on a dataset.
---
+++ m1/stats.h
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h 
+ * @brief Header file
+ *
+ * The stats.h header file includes declarations and documentation for the 
+ * functions from the stats.c file. 
+ *
+ *
+ * @author Devika Nair
+ * @date 15-05-2021
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+
+ 
+
+
+#endif /* __STATS_H__ */
