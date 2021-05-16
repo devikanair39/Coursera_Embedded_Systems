b05b41c816d3ac2b3d5e9b0c238ec7d9e96c349e
Devika <devikanair3999@gmail.com>
2021-05-16 20:52:40 +0530

    Adding actual feature developments for C1 M1 assignment

--- m1/stats.c
+++ m1/stats.c
@@ -40,14 +40,118 @@
 	  /* Other Variable Declarations Go Here */
 	   
 	  /* Statistics and Printing Functions Go Here */
-      //print_statistics(test,SIZE);
+      print_statistics(test,SIZE);
 		
 	  
 	}
 	
 	/* Add other Implementation File Code Here */
 	
+	void print_statistics(unsigned char arr[], unsigned int length)
+	  {
+	  	printf("The original array:\n");
+		print_array(arr, length);
+		sort_array(arr, length);
+		printf("The Sorted array:\n");
+		print_array(arr, length);
+	  	printf("The mean of the input data is : %d \n",find_mean(arr,length));
+	  	printf("\n");
+	  	printf("The median of the input data is : %d \n",find_median(arr,length));
+	  	printf("\n");
+	  	printf("The maximum number from the input data is : %d \n",find_maximum(arr,length));
+	  	printf("\n");
+	  	printf("The minimum number from the input data is : %d \n",find_minimum(arr,length));
+	  	printf("\n");
+
+	  }
+	 
+	  void print_array(unsigned char arr[], unsigned int length)
+	  {
+	  	int i;                            
+	  	int j;                       
+	  	for(i = 0; i < SIZE/8; i++)                  //row formatting               
+	  	{
+	  		for(j = 0; j < SIZE/5; j++)             //column formatting
+	  		{
+	  			printf("%3d ",arr[8*i + j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+	  }
 	  
+	  unsigned char find_median(unsigned char arr[], unsigned int length)
+	  {
+	     int n =(length+1)/2 -1;      //-1 because the array indexing starts from -1
+	  	 return arr[n];
+	  }
+	  
+	 unsigned char find_mean(unsigned char arr[], unsigned int length)
+	 {
+	 	int mean;
+	 	int sum = 0;
+	 	int i;
+	 	for(i = 0;i<length;i++)
+	 	{
+	 		sum += arr[i];	
+		}
+		mean = round(sum/length);
+		return (unsigned char) mean;     //typecasting
+	 }
+	 
+	
+	  void sort_array(unsigned char arr[], unsigned int length)
+	 {
+	 	unsigned char temp;
+	 	int i;
+	 	int j;
+	 	//int max_idx;
+	 	for(i = 0; i < length; i++)
+	 	{
+	 		for(j = i+1; j < length; j++)
+	 		{
+	 			
+	 			if(arr[i] < arr[j])
+	 			{
+	 				temp = arr[i] ;
+					arr[i] = arr[j];
+					arr[j] = temp;	
+				}
+			}
+		}
+		
+	 }
+	 
+	 unsigned char find_minimum(unsigned char arr[], unsigned int length)
+	 {
+	 	int i;
+	 	int min = arr[0];
+	 	for(i = 1; i < length; i++)
+	 	{
+	 		if(min > arr[i])
+	 		{
+	 			min = arr[i];
+			}
+	 		
+		}
+	 	return (unsigned char)min;
+	 }
+	 
+	 unsigned char find_maximum(unsigned char arr[], unsigned int length)
+	 {
+	 	int i;
+	 	int max = arr[0];
+	 	for(i = 1; i < length; i++)
+	 	{
+	 		if(max < arr[i])
+	 		{
+	 			max = arr[i];
+			}
+		}
+		return (unsigned char)max;
+	 }
+	
+
 		
 
 
